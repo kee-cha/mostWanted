abdaba2b14a31fd1beb056a5f284417ab27568a2
kee cha <kee_cha18@yahoo.com>
2019-11-19 07:09:47 -0600

    deleted comments off the file

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 "use strict"
-/*
-Build all of your functions for displaying and gathering information below (GUI).
-*/
-
-// app is the function called to start the entire application
 function app(people) {
   let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   let searchResults;
@@ -12,7 +7,6 @@ function app(people) {
       searchResults = searchByName(people);
       break;
     case 'no':
-      // TODO: search by traits
       let searchGender = promptFor("Do you want to search by trait?", yesNo).toLocaleLowerCase();
       let foundSearch;
       switch (searchGender) {
@@ -29,22 +23,16 @@ function app(people) {
       app(people)
       break;
     default:
-      app(people); // restart app
+      app(people);
       break;
   }
-
-  // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
   mainMenu(searchResults, people);
 }
 
-// Menu function to call once you find who you are looking for
 function mainMenu(person, people) {
-
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-
   if (!person) {
     alert("Could not find that individual.");
-    return app(people); // restart
+    return app(people);
   }
 
   let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
@@ -60,7 +48,6 @@ function mainMenu(person, people) {
         case "no":
           break;
       }
-      // TODO: get person's info
       break;
     case "family":
       spouse(person, people);
@@ -74,10 +61,8 @@ function mainMenu(person, people) {
         case "no":
           break;
       }
-      // TODO: get person's family
       break;
     case "descendants":
-      // TODO: get person's descendants
       children(person, people);
       let answer = promptFor("Would you like to start a new search? Enter yes or no.", yesNo).toLocaleLowerCase();
       switch (answer) {
@@ -89,12 +74,12 @@ function mainMenu(person, people) {
       }
       break;
     case "restart":
-      app(people); // restart
+      app(people);
       break;
     case "quit":
-      return; // stop execution
+      return;
     default:
-      return mainMenu(person, people); // ask again
+      return mainMenu(person, people);
   }
 }
 
@@ -129,7 +114,6 @@ function spouse(person, people) {
   return foundFam;
 }
 
-// alerts a list of people
 function displayPeople(people) {
   alert(people.map(function (person) {
     return person.firstName + " " + person.lastName;
@@ -137,9 +121,6 @@ function displayPeople(people) {
 }
 
 function displayPerson(person) {
-
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
   let personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
   personInfo += "ID Number: " + person.id + "\n";
@@ -149,12 +130,9 @@ function displayPerson(person) {
   personInfo += "Weight: " + person.weight + "\n";
   personInfo += "Eye Color: " + person.eyeColor + "\n";
   personInfo += "Occupation: " + person.occupation + "\n";
-  // TODO: finish getting the rest of the information to display
-
   alert(personInfo);
 }
 
-// function that prompts and validates user input
 function promptFor(question, valid) {
   do {
     var response = prompt(question).trim();
@@ -162,14 +140,12 @@ function promptFor(question, valid) {
   return response;
 }
 
-// helper function to pass into promptFor to validate yes/no answers
 function yesNo(input) {
   return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
 }
 
-// helper function to pass in as default promptFor validation
 function chars(input) {
-  return true; // default validation only
+  return true;
 }
 
 function boyGirl(input) {
@@ -203,7 +179,7 @@ function parents(person, people) {
 function siblings(person, people) {
   let foundFam3 = people.filter(function (people) {
     if (people.parents.length < 1) {
-    } else if (people.parents[0] === person.parents[0]) { 
+    } else if (people.parents[0] === person.parents[0]) {
       if (person.id === people.id) {
         return false;
       } else {
@@ -387,4 +363,3 @@ function job(input) {
       break;
   }
 }
-
